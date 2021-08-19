6d1cf35a198f6be32efed7d137b1117996b8a707
Jeff Salisbury <salisbury.jeffery@gmail.com>
2021-08-18 22:58:41 -0400

    Added key handler and can move character about in the game loop and exit the game.

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,35 @@ struct Tcod {
     root: Root,
 }
 
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+    // todo: handle keys
+    let key = tcod.root.wait_for_keypress(true);
+
+    match key {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true, // exit game
+        // Movement Keys
+        Key {code: Up, ..} => *player_y -=1,
+        Key {code: Down, ..} => *player_y +=1,
+        Key {code: Left, ..} => *player_x -=1,
+        Key {code: Right, ..} => *player_x +=1,
+        // Every other key
+        _=>{}
+    }
+
+    false
+}
+
 fn main() {
     let root: Root = Root::initializer()
         .font("prestige10x10_gs_tc.png", FontLayout::Tcod)
@@ -22,11 +51,20 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root
+        .put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
+        // Key Handler and exit game
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
 }
